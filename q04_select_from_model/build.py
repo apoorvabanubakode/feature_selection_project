439c727377c4ba888f04179cc65b514729748b43
apoorvabanubakode <apoorva.banubakode@gmail.com>
2017-10-27 19:44:22 +0000

    Done

--- q04_select_from_model/build.py
+++ q04_select_from_model/build.py
@@ -7,4 +7,22 @@ import numpy as np
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
+#data = pd.read_csv('data/house_prices_multivariate.csv')
+
+def select_from_model(df):
+    features=df.iloc[:,:-1]
+    target=df.iloc[:,-1]
+
+    select=SelectFromModel(RandomForestClassifier(random_state=9))
+    x_new=select.fit_transform(features,target)
+    support=select.get_support()
+    #print support
+    finalfeature=[]
+    for i,feature in enumerate(features):
+        if support[i]==True:
+            finalfeature.append(feature)
+    return finalfeature
+
+# Your solution code here
+
 # Your solution code here
